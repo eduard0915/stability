e68bd1d3d0864a7667f7f91c60131705800b4492
eduard0915 <edd.alvis@gmail.com>
2021-10-17 16:58:43 -0500

    Disminucion tamaño foto de user logeado

--- config/settings.py
+++ config/settings.py
@@ -23,16 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&c0!66*-%_$eq5^disnp#@pklamgj29qslu)3jej^b!99)0i92'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.bpmpro.co']
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
